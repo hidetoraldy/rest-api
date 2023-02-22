75e4e063d14e9a12e8d6241a8d2fbd6089a39f18
hidetoraldy <hidetoraldy@gmail.com>
2023-02-22 15:05:40 +0700

    berlatih sampai di latihan2

---
+++ json/latihan1.php
@@ -0,0 +1,17 @@
+<?php 
+    $mahasiswa = [
+        [
+            "nama"  => "Aldy Ramadhan",
+            "NIM"   => "10517082",
+            "email" => "hidetoraldy@gmail.com"
+        ],
+        [
+            "nama"  => "Aldy Ramadhan",
+            "NIM"   => "10517082",
+            "email" => "hidetoraldy@gmail.com"
+        ]
+    ];
+
+    $data = json_encode($mahasiswa);
+    echo $data;
+?>
